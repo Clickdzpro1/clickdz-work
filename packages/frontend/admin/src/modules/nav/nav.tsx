94a7616016ebc6f9691409ff41bfdff17444d2f6
Clickdzpro1 <clickdz.usa@gmail.com>
2026-08-10 07:39:24 +0100

    fix(admin): remove Surveillance nav item temporarily (monitoring route disabled)

--- packages/frontend/admin/src/modules/nav/nav.tsx
+++ packages/frontend/admin/src/modules/nav/nav.tsx
@@ -5,7 +5,6 @@ import {
   BarChart3Icon,
   ChartPieIcon,
   FlagIcon,
-  HeartPulseIcon,
   LayoutDashboardIcon,
   ListChecksIcon,
   AppWindowIcon,
@@ -55,12 +54,6 @@ export function Nav({ isCollapsed = false }: NavProps) {
           to={ROUTES.admin.analytics}
           icon={<ChartPieIcon size={18} />}
           label="Analytique"
-          isCollapsed={isCollapsed}
-        />
-        <NavItem
-          to={ROUTES.admin.monitoring}
-          icon={<HeartPulseIcon size={18} />}
-          label="Surveillance"
           isCollapsed={isCollapsed}
         />
         {environment.isSelfHosted ? null : (
